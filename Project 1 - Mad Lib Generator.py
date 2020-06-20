86937564cda16d53905aee89d6b7a2df1377500d
Kenspectacle <Kennyftw2@gmail.com>
2020-06-20 19:43:57 +0200

    Initial commit

---
+++ Project 1 - Mad Lib Generator.py
@@ -0,0 +1,15 @@
+if __name__ == "__main__":
+
+    Adjective = ""
+    Noun1 = ""
+    Noun2 = ""
+    Adverb = ""
+    print("Hello, welcome to your(mine?) first Python Project! :)\nIn this project we will be doing madlib. Have fun! :D")
+    Adjective = input("Please Input an adjective: ")
+    Noun1 = input("Please Input the first noun: ")
+    Noun2 = input("Please Input the second noun: ")
+    print("\nalmost there....\n")
+    Adverb = input("Please Input an Adverb: ")
+    print(f"You have found a {Noun1} in the {Adjective} Valhalla!!\n Congratulation, but it is hardly {Adverb} compared to {Noun2}. Keep going!")
+
+    
